7c8b206ed51997f1efd6607ace993a5482e717bb
Sherv <ssahe002@ucr.edu>
2020-04-15 18:44:03 -0700

    Completed part 4

--- turnin/ssahe002_lab3_part5.c
+++ turnin/ssahe002_lab3_part5.c
@@ -14,34 +14,33 @@
 #endif
 
 int main(void) {
-	DDRD = 0x00; PORTD = 0xFF;
 	DDRB = 0xFE; PORTB = 0x01;
-	
+	DDRD = 0x00; PORTD = 0xFF;	
 	unsigned short weight = 0x0000;
 	unsigned char tmp = 0x00;
 	unsigned char out = 0x01;
-    while(1)
-    {
-        weight = (PIND << 1);
+
+while(1) {
+	weight = (PIND << 1);
 	tmp = PINB & 0x01;
 	
 	weight = weight | tmp;
 	
 //	out = (weight >= 70) ? 0x02 : out = (weight > 5 && weight < 70) ? 0x04 : 0x00;
 
-	if (weight >= 0x0046) {
+	if (weight >= 70) {
 		out = 0x02;
 	}
-	else if ((weight < 0x0046) && (weight > 0x0055)) {
+	else if ((weight > 5) && (weight < 70)) {
 		out = 0x04;
 	}
 
 
 	PORTB = out;
-	out = 0x01;
-	weight = 0x0000;
-	}	
-return 1;
+	out = 0x00;
+	weight = 0x00;
+	}
+	return 1;
 }
 
 
