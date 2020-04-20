7869f3731ac5e39cb47a6b880149363cb6efaad8
Sherv <ssahe002@ucr.edu>
2020-04-19 18:15:57 -0700

    Completed part 2

--- turnin/ssahe002_lab4_part2.c
+++ turnin/ssahe002_lab4_part2.c
@@ -14,99 +14,91 @@
 #include "simAVRHeader.h"
 #endif
 
-enum States {Start, Init, INCREMENT, DECREMENT, WAIT, RESET}state;
+enum States {Start, INCREMENT, DECREMENT, ZERO}state;
 void Tick();
 
 void Tick(){
 	switch(state){ //Transitions
 		case Start:
-			state = Init;
-			break;
-			
-		case Init:
-		if((~PINA & 0x03) == 0x01) {	//increment
-			state = INCREMENT;
-		       	break;
-		}
-
-		else if((~PINA & 0x03) == 0x02) {	//decrement
-			state = DECREMENT; 
-			break;
-		}
-
-		else if((~PINA & 0x03) == 0x03) {	//reset
-			state = RESET; 
-			break;
-		}
-
-		else {
-			state = Init; 
-			break;
-		}
-		
-		case INCREMENT:
-		state = WAIT;
-		break;
-		
-		case DECREMENT:
-		state = WAIT;
-		break;
-		
-		case WAIT:
-		if (((~PINA & 0x03) == 0x01) || ((~PINA & 0x03) == 0x02)) {
-			state = WAIT; break;
-		}
-		else if((~PINA & 0x03) == 0x03)
-		{
-			state = RESET; break;
-		}
-		else
-		{
-			state = Init; break;
-		}
-		
-		case RESET:
-		state = (((~PINA & 0x03) == 0x01) || ((~PINA & 0x03) == 0x02)) ? RESET : Init;
-		break;
-	
-		
-		default:
-		break;
-	}
-
-	switch(state){ //State actions
-		case Start:
+			state = ZERO;
 			PORTC = 0x07;
 			break;
-		
-		case Init:
-			break;
-		
+			
 		case INCREMENT:
-			PORTC = (PORTC >= 0x09) ? 0x09 : PORTC + 0x01;
+
+			if((PINA & 0x0F) == 0x03) {	//increment
+				state = ZERO;
+		       		PORTC = 0;
+			}
+
+			else if((PINA & 0x0F) == 0x01) {	//decrement
+				state = INCREMENT;
+			}
+	
+			else if((PINA & 0x0F) == 0x00) {	//reset
+				state = ZERO; 
+			}
+	
+			else {
+				PORTC = PORTC; 
+			}
 			break;
 		
 		case DECREMENT:
-			PORTC = (PORTC <= 0x00) ? 0x00 : PORTC - 0x01;
+			if ((PINA & 0x0F) == 0x03) {
+				state = ZERO;
+				PORTC = 0;
+			}
+			else if ((PINA & 0x0F) == 0x02) {
+				state = DECREMENT;
+			}
+			else if ((PINA & 0x0F) == 0x00) {
+				state = ZERO;
+			}
+			else {
+				PORTC = PORTC;
+			}
 			break;
-		
-		case WAIT:
+
+		default:
+			PORTC = 0x07;
+			state = Start;
 			break;
-		
-		case RESET:
-			PORTC = 0x00; 
+
+		case ZERO:
+			if ((PINA & 0x0F) == 0x03) {
+				state = ZERO;
+				PORTC = 0;
+			}
+
+			else if ((PINA & 0x0F) == 0x01) {
+				state = INCREMENT;
+				if (PORTC < 9) {
+					PORTC++;
+				}
+			}
+
+			else if ((PINA & 0x0F) == 0x02) {
+				state = DECREMENT;
+				if (PORTC > 0) {
+					PORTC--;
+				}
+			}
+			else {
+				PORTC = PORTC;
+			}
 			break;
-		}
-}
-int main(void)
-{
-	DDRA = 0x00; PORTA = 0xFF;
-	DDRC = 0xFF; PORTC = 0x07;
-	state = Start;
-	while(1)
-	{
-		Tick();
 	}
 }
+	int main(void) {
 
+		DDRA = 0x00; PORTA = 0xFF;
+		DDRC = 0xFF; PORTC = 0x00;
+		state = Start;
+	
+		while(1) {
+			Tick();
+		}
+		return 0;
+	}
 
